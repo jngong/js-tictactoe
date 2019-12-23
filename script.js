bd2c4116bb398e21beefd05b608c0ea70f0baec0
jngong <34215359+jngong@users.noreply.github.com>
2019-12-23 11:33:00 -0500

    added pseudocode for initial game logic

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('ready')
+/* 
+Pseudocode
+Start by adding an event listener to each square.
+With each click on a square, we will add a class and change the innerHTML to either X or O depending on the turn.
+With each click on a square, the player changes.
+Once a square has been clicked and changed, it cannot be clicked and changed again.
+A player wins if/when:
+- all squares in a row are the same
+- all squares in a column are the same
+- box 1, box 5, box 9 are the same
+- box 3, box 5, box 7 are the same
+If all boxes are clicked and none of the above apply, then there is no winner
+*/
+
+let squares = document.querySelectorAll('.square')
+console.log(squares)
+
+squares.forEach(square => {
+    square.addEventListener('click', assignClass)
+})
+
+function assignClass() {
+    console.log(`adding class`)
+}
