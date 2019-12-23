2d41986307dd739e501c2f55a6d00e313d58b9dd
jngong <34215359+jngong@users.noreply.github.com>
2019-12-23 18:57:47 -0500

    working on game logic

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ With each click on a square, we will add a class and change the innerHTML to eit
 With each click on a square, the player changes. DONE
 Keep track of # moves. DONE
 
-Once a square has been clicked and changed, it cannot be clicked and changed again. ???
+Once a square has been clicked and changed, it cannot be clicked and changed again. DONE
 
 A player wins if/when:
 - all squares in a row are the same
@@ -17,6 +17,7 @@ If all boxes are clicked and none of the above apply, then there is no winner.
 
 */
 
+//define all variables - track players, number of moves, access DOM elements
 let playerX = true;
 let playerO = false;
 let moves = 9;
@@ -37,45 +38,45 @@ let box7 = document.querySelector('#box7')
 let box8 = document.querySelector('#box8')
 let box9 = document.querySelector('#box9')
 
-squares.forEach(square => {
-    square.addEventListener('click', assignClass)
-})
 
-function assignClass() {
-    if (playerX && moves > 0) {
-        this.classList.add('playerX')
-        this.innerHTML = 'X'
-        playerX = false
-        playerO = true
-        moves -= 1
-        console.log(moves)
-    } else if (playerO && moves > 0) {
-        this.classList.add('playerO')
-        this.innerHTML = 'O'
-        playerO = false
-        playerX = true
-        moves -= 1
-        console.log(moves)
-    }
-    checkWinner();    
-}
-
-reset.addEventListener('click', resetGame)
-
-function resetGame() {
+function newGame() {
+    playerX = true
+    playerO = false
+    moves = 9
+    
     squares.forEach(square => {
         square.classList.remove('playerX')
         square.classList.remove('playerO')
         square.innerHTML = ''
+        square.addEventListener('click', assignClass)
     })
-    playerX = true
-    playerO = false
-    moves = 9
+    
+    function assignClass() {
+        if (playerX && moves > 0) {
+            this.classList.add('playerX')
+            this.innerHTML = 'X'
+            playerX = false
+            playerO = true
+            moves -= 1
+            console.log(moves)
+            this.removeEventListener('click', assignClass)
+        } else if (playerO && moves > 0) {
+            this.classList.add('playerO')
+            this.innerHTML = 'O'
+            playerO = false
+            playerX = true
+            moves -= 1
+            console.log(moves)
+            this.removeEventListener('click', assignClass)
+        }
+        console.log(`Player X: ${playerX}, Player O: ${playerO}`)    
+        checkWinner();
+    }
     console.log(`Reset. Moves: ${moves}, PlayerX: ${playerX}, PlayerO: ${playerO}`)
 }
 
 function checkWinner() {
-    switch(true && moves > 0) {
+    switch(true) {
         case box1.classList.contains('playerX') && box2.classList.contains('playerX') && box3.classList.contains('playerX'): 
         console.log(`player X wins`);
         break; 
@@ -125,7 +126,13 @@ function checkWinner() {
         console.log(`player O wins`);
         break;
         default:
-            console.log('no winner')
+            if (moves === 0) {
+                console.log('no winner')
+            }
+        }
+        
     }
+    
+    reset.addEventListener('click', newGame)
 
-}
+    newGame();
